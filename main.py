7f39b72b954ca138744538c0c96564016d6e9683
sptvhelpdesk-ship-it <sptvhelpdesk@gmail.com>
2026-04-04 15:46:52 +0530

    Update Referer header URL in main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def update_json():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Referer": "https://allinonereborn.online/",
+        "Referer": "https://allinonereborn-jtv.pages.dev/",
         "Accept-Language": "en-US,en;q=0.9"
     }
 
